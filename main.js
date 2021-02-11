7581267bb0e8ed5e22b6671e9a8053b8e92d28fd
Ahsanul Insan Hamid <ahsanulinsanhamid@gmail.com>
2021-02-11 10:48:32 +0900

    Update script supaya functions jadi lebih modular

--- main.js
+++ main.js
@@ -1,22 +1,41 @@
+// Fungsi event listener setiap checkbox dan peta provinsi
+for (let i = 1; i <= 34; i++) {
+    let formattedNumber = ('0' + i).slice(-2);
+    document.getElementById(`checkbox${formattedNumber}`).addEventListener('click', checkboxChecked, false);
+    document.getElementById(`provinsi${formattedNumber}`).addEventListener('click', petaClicked, false);
+    document.getElementById(`provinsi${formattedNumber}`).addEventListener('mouseover', mouseOver, false);
+    document.getElementById(`provinsi${formattedNumber}`).addEventListener('mouseout', mouseOut, false);
+}
+
+// Fungsi mewarnai peta
+function mewarnai(id) {
+    document.getElementById(`provinsi${id}`).style.fill = '#80ed99';
+}
+
+// Fungsi hapus warna (ganti warna jadi default)
+function hapusWarna(id) {
+    document.getElementById(`provinsi${id}`).style.fill = '#ced4da';
+}
+
 // Mengecek apakah checkbox checked atau tidak
 function checkboxChecked() {
-    let angka = this.id[this.id.length - 2] + this.id[this.id.length - 1];
+    let angka = this.id.slice(-2);
     if (document.getElementById(this.id).checked) {
-        document.getElementById(`provinsi${angka}`).style.fill = '#80ed99';
+        mewarnai(angka);
     } else {
-        document.getElementById(`provinsi${angka}`).style.fill = '#ced4da';
+        hapusWarna(angka);
     }
     kalkulasi();
 }
 
 // Mengecek apakah peta terclick atau tidak
 function petaClicked() {
-    let angka = this.id[this.id.length - 2] + this.id[this.id.length - 1];
+    let angka = this.id.slice(-2);
     if (!document.getElementById(`checkbox${angka}`).checked) {
-        document.getElementById(this.id).style.fill = '#80ed99';
+        mewarnai(angka);
         document.getElementById(`checkbox${angka}`).checked = true;
     } else {
-        document.getElementById(this.id).style.fill = '#ced4da';
+        hapusWarna(angka);
         document.getElementById(`checkbox${angka}`).checked = false;
     }
     kalkulasi();
@@ -24,14 +43,15 @@ function petaClicked() {
 
 // Fungsi mouseover peta
 function mouseOver() {
-    document.getElementById(this.id).style.fill = '#80ed99';
+    let angka = this.id.slice(-2);
+    mewarnai(angka);
 }
 
 // Fungsi mouseout peta
 function mouseOut() {
-    let angka = this.id[this.id.length - 2] + this.id[this.id.length - 1];
+    let angka = this.id.slice(-2);
     if (!document.getElementById(`checkbox${angka}`).checked) {
-        document.getElementById(this.id).style.fill = '#ced4da';
+        hapusWarna(angka);
     }
 }
 
@@ -42,30 +62,21 @@ function kalkulasi() {
     document.getElementById('sum').innerText = `Kamu sudah pernah mengunjungi ${jumlahchecked.length}/34 provinsi di Indonesia! (${persen}%)`;
 }
 
-// Fungsi event listener setiap checkbox dan peta provinsi
-for (let i = 1; i <= 34; i++) {
-    let formattedNumber = ("0" + i).slice(-2);
-    document.getElementById(`checkbox${formattedNumber}`).addEventListener("click", checkboxChecked, false);
-    document.getElementById(`provinsi${formattedNumber}`).addEventListener("click", petaClicked, false);
-    document.getElementById(`provinsi${formattedNumber}`).addEventListener("mouseover", mouseOver, false);
-    document.getElementById(`provinsi${formattedNumber}`).addEventListener("mouseout", mouseOut, false);
-}
-
 // Fungsi tab
-document.getElementById("defaultOpen").click();
+document.getElementById('defaultOpen').click();
 function openPulau(evt, pulauName) {
     var i, tabcontent, tablinks;
 
-    tabcontent = document.getElementsByClassName("tabcontent");
+    tabcontent = document.getElementsByClassName('tabcontent');
     for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+        tabcontent[i].style.display = 'none';
     }
 
-    tablinks = document.getElementsByClassName("tablinks");
+    tablinks = document.getElementsByClassName('tablinks');
     for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].className = tablinks[i].className.replace(' active', '');
     }
 
-    document.getElementById(pulauName).style.display = "block";
-    evt.currentTarget.className += " active";
+    document.getElementById(pulauName).style.display = 'block';
+    evt.currentTarget.className += ' active';
 }
